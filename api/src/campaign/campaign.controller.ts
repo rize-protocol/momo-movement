794e3d1abc223225199c15cd65a7ebf9749ec3dc
sswsdsn <hrblhf08@gmail.com>
2024-08-13 13:43:26 +0800

    add galxe redirect api

--- api/src/campaign/campaign.controller.ts
+++ api/src/campaign/campaign.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Redirect } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
 import { User } from 'movement-gaming-model';
 import { EntityManager } from 'typeorm';
@@ -8,11 +8,14 @@ import { BindGalxeRequest } from '@/campaign/dto/bind-galxe.request';
 import { BindReferralInternalRequest } from '@/campaign/dto/bind-referral-internal.request';
 import { CheckGalxeAddressRequest } from '@/campaign/dto/check-galxe-address.request';
 import { CheckSecure3Request } from '@/campaign/dto/check-secure3.request';
+import { RedirectGalxeRequest } from '@/campaign/dto/redirect-galxe.request';
 import { Admin, Public } from '@/common/decorators/auth.decorator';
 import { CurrentUser } from '@/common/decorators/current-user.decorator';
 
 @Controller('campaign')
 export class CampaignController {
+  private readonly galxeRedirectUrl = 'https://t.me/MomoByRizeBot/momo?startapp=';
+
   constructor(
     @InjectEntityManager() private readonly entityManager: EntityManager,
     private readonly campaignService: CampaignService,
@@ -47,6 +50,16 @@ export class CampaignController {
   @Get('galxe_check')
   async galxeCheck(@Query() request: CheckGalxeAddressRequest) {
     return this.campaignService.galxeCheck(this.entityManager, request.address);
+  }
+
+  @Public()
+  @Get('galxe_redirect')
+  @Redirect()
+  async galxeRedirect(@Query() request: RedirectGalxeRequest) {
+    const { code, state } = request;
+    const redirectUrl = `${this.galxeRedirectUrl}${code}&state=${state}`;
+
+    return { url: redirectUrl, statusCode: 302 };
   }
 
   @Public()
